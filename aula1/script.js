4372c4bc053d5e0869a2ef970b8545ce52c951ab
MathiasDev-stack <mathiasprogrammer@gmail.com>
2023-02-12 17:16:24 -0300

    Create script.js

---
+++ aula1/script.js
@@ -0,0 +1,37 @@
+console.log("hello world")
+
+{
+    let nome ="Evandro Mathias"
+    console.log(nome)
+}
+//let e uma variavel que so funciona dentro do escopo
+
+{
+    var nome2="Evandro Mathias"
+}
+console.log(nome2)
+
+//quando se cria let fora do escopo ele pode ser executado fora
+
+let nome3 = "Evandro Mathias"
+
+console.log(nome3)
+
+//tipos de variaveis
+
+//number
+//let n1=16
+//let n2=7.5
+
+//string
+//let nome 4="Evandro Mathias"
+
+//booleans
+//let v1=true
+//let v2-false
+
+//array objects
+//let carros=("volks","fiat","ford")
+
+//date object
+//let date =newdate("2023-02-12")
