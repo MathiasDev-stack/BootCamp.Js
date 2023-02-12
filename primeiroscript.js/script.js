2cb6a1a0301e36e068abf31ea0dcb5a701c37998
MathiasDev-stack <mathiasprogrammer@gmail.com>
2023-02-12 17:02:32 -0300

    Update script.js

--- primeiroscript.js/script.js
+++ primeiroscript.js/script.js
@@ -1,1 +1,18 @@
 console.log("hello world")
+
+{
+    let nome ="Evandro Mathias"
+    console.log(nome)
+}
+//let e uma variavel que so funciona dentro do escopo
+
+{
+    var nome2="Evandro Mathias"
+}
+console.log(nome2)
+
+//quando se cria let fora do escopo ele pode ser executado fora
+
+let nome3 = "Evandro Mathias"
+
+console.log(nome3)
