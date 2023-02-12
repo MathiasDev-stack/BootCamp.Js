d044429c3ae712d214ad01f28036b9e3c2e584b0
MathiasDev-stack <mathiasprogrammer@gmail.com>
2023-02-12 17:14:37 -0300

    Update script.js

--- primeiroscript.js/script.js
+++ primeiroscript.js/script.js
@@ -16,3 +16,22 @@ console.log(nome2)
 let nome3 = "Evandro Mathias"
 
 console.log(nome3)
+
+//tipos de variaveis
+
+//number
+//let n1=16
+//let n2=7.5
+
+//string
+//let nome 4="Evandro Mathias"
+
+//booleans
+//let v1=true
+//let v2-false
+
+//array objects
+//let carros=("volks","fiat","ford")
+
+//date object
+//let date =newdate("2023-02-12")
