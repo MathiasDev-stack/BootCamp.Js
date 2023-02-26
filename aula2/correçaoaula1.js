40b336148b493572f05e99baac8f951a03011408
MathiasDev-stack <mathiasprogrammer@gmail.com>
2023-02-26 13:40:57 -0300

    update

---
+++ aula2/correçaoaula1.js
@@ -0,0 +1,14 @@
+ //montar um sistema que leia 4 valores e imprima a media
+
+ {
+    let n1 = prompt("informe N1")
+    let n2 = prompt("informe N2")
+    let n3 = prompt("informe N3")
+    let n4 = prompt("informe N4")
+
+    let media = (parsefloat(n1) + parseFloat(n2) + parseFloat(n3) + parseFloat(n4)) /4
+
+    console.log(media)
+    alert(media)
+
+ }
